701b28023e17f214cbe6e499d25e8fc67c2cf1fb
nastya-dana <102735585+nastya-dana@users.noreply.github.com>
2024-02-07 19:12:22 +0300

    layout/main-page1

---
+++ delivery_restaurant/src/components/elements/CardProduct.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">card</div>
+    <ButtonX />>
+</template>
+
+<script>
+// import { ref } from 'vue'
+import ButtonX from '@/components/ui/ButtonX.vue'
+export default {
+    name: 'CardProduct',
+    components: {
+        ButtonX
+    },
+    props: {
+    },
+    setup() {
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
